06350eed1c39bc1cfb121133f640a0068e18f319
Christian Amauger <christian.amauger@gmail.com>
2019-07-16 14:52:12 -0400

    searchbar component basic design

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import SearchBar from "./components/SearchBar";
+import SearchBar from "./SearchBar";
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className="ui container">
         <SearchBar />
       </div>
     );
